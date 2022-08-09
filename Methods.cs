dac6d8c5b781cc19bbeac0fa4ef558f5bdc08974
ezeleke <abefikru@gmail.com>
2022-08-09 14:39:39 -0700

    OperatorExercise first commit

---
+++ Methods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorExercise
+{
+    internal class Methods
+    {
+        
+        public static int Addition(int a, int b)
+        {
+             a = 17;
+             b = 4;
+            int sum;
+            sum = a + b;
+            return sum;
+        }
+        public static int Multiply(int a, int b) 
+        {
+            int total;
+            total = a * b;
+            return total;
+        }
+        public static void Division(int a, int b) 
+        {             
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b== 4) 
+            {
+                Console.WriteLine($"{a} is == to {a}");
+                Console.WriteLine($"{a} /= {b} is {quotient} remainder of {remainder}");
+               
+            }
+           
+        }
+        public Double AreaOfCircle(double radius) 
+        {
+             
+            return Math.PI * Math.Pow(radius, 2);
+
+            
+            
+           
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var methods = new Methods();
+             Console.WriteLine("Hello World!");
+             Methods.Division(17, 4);
+            //Methods.Division(12, 3);
+            //Console.WriteLine("What is the radius of your circle?");
+            //var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine("What is the radius of your circle?");
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+            Console.WriteLine(methods.AreaOfCircle(radius));
+
+
+
+        }
+    }
+}
